20d63577d5dc375cb45051f172e97afd3ccf9891
fatihfilizol <fatihfilizol@gmail.com>
2022-11-13 10:53:40 +0300

    Initial Commit

---
+++ JokesApp/JokesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesAppApp.swift
+//  JokesApp
+//
+//  Created by Fatih Filizol on 13.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct JokesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
